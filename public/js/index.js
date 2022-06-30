59a6b3a668480eef91c3bd5954d8e18044bdb9f8
Micka <pro.djoghlal@gmail.com>
2022-06-30 18:27:27 +0200

    start project

---
+++ public/js/index.js
@@ -0,0 +1,35 @@
+const clockDay = function() {
+    let date = new Date();
+
+    let hour = date.getHours();
+    let min = date.getMinutes();
+    let sec = date.getSeconds();
+
+    let day = date.getDay();
+    let month = date.getMonth();
+    let year = date.getFullYear();
+  
+    if (sec < 10) {
+        sec = '0' + sec;
+    }
+  
+    if (min < 10) {
+        min = '0' + min;
+    }
+
+    if (day < 10) {
+        day = '0' + day;
+    }
+
+    if (month < 10) {
+        month = '0' + month;
+    }
+  
+    let displayHour = hour + ":" + min + ":" + sec;
+    let displayDate = day + "/" + month + "/" + year;
+  
+    document.querySelector("#clock").innerText = displayHour;
+    document.querySelector("#date").innerText = displayDate;
+  };
+  
+  window.setInterval(clockDay, 1000);
